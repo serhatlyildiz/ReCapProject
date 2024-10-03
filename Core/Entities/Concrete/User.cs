ccda538586cbd1a95f7fbb97012cd4ca60d613f0
Serhat Yıldız <serhat.yildiz2@gazi.edu.tr>
2024-10-03 17:12:53 +0300

    14. odev sonu

--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -1,13 +1,10 @@
-﻿using Core.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Entities.Concrete
+namespace Core.Entities.Concrete
 {
-    public class Users : IEntity
+    public class User : IEntity
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
--- DataAccess/Concrete/EntityFramework/EfUsersDal.cs
+++ DataAccess/Concrete/EntityFramework/EfUsersDal.cs
@@ -1,4 +1,5 @@
 ﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
@@ -6,11 +7,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using User = Core.Entities.Concrete.User;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfUsersDal : EfEntityRepositoryBase<Users, ReCapContext>, IUsersDal
+    public class EfUserDal : EfEntityRepositoryBase<User, ReCapContext>, IUserDal
     {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            using (var context = new ReCapContext())
+            {
+                var result = from operationClaim in context.OperationClaims
+                             join userOperationClaim in context.UserOperationClaims
+                                 on operationClaim.Id equals userOperationClaim.OperationClaimId
+                             where userOperationClaim.UserId == user.Id
+                             select new OperationClaim { Id = operationClaim.Id, Name = operationClaim.Name };
+                return result.ToList();
 
+            }
+        }
     }
 }
